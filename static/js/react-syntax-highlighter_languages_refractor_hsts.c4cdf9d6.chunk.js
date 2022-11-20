b8bd1409b1f368bbfb63275d55e98e8acc9dddd6
Artem Tarasov <art.tarasov0@gmail.com>
2022-11-20 21:12:42 +0400

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.c4cdf9d6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklearn_js=self.webpackChunklearn_js||[]).push([[3140],{7761:function(a){function e(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=e,e.displayName="hsts",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.c4cdf9d6.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.0e928b1a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklearn_js=self.webpackChunklearn_js||[]).push([[6495],{3409:function(t){function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.0e928b1a.chunk.js.map
